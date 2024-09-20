72455de70f67cef4714e14b519f2a9e7a2eeaef0
shiryay <segeshi@yandex.ru>
2024-09-20 19:18:50 +0300

    provided for adding telegram bot modewith flags

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"validator/helper"
+	"os"
+	"validator/processor"
 )
 
 func main() {
+	var mode string
+
+	fs := flag.NewFlagSet("mode", flag.ExitOnError)
+	fs.StringVar(&mode, "m", "", "server type")
+	if len(os.Args) < 3 {
+		fs.Usage()
+		return
+	}
+	err := fs.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Println("Error parsing flag: ", err)
+	} else if mode == "web" {
+		fmt.Println("Running in web mode")
+	} else if mode == "tg" {
+		fmt.Println("Running as telegram bot")
+	} else {
+		fmt.Println("Unknown mode")
+	}
+}
+
+func setupWebServer() {
 	// Serve static files from the "static" directory
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
@@ -16,7 +39,6 @@ func main() {
 	// Start the server on port 8080
 	fmt.Println("Server started at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
-
 }
 
 // handleSubmit is called when the button is clicked
@@ -27,7 +49,7 @@ func handleSubmit(w http.ResponseWriter, r *http.Request) {
 
 		// Send a response back to the client
 		if inputText != "" {
-			fmt.Fprint(w, helper.CheckText(inputText))
+			fmt.Fprint(w, processor.CheckText(inputText))
 		} else {
 			fmt.Fprint(w, "You have to provide the text to check")
 		}
