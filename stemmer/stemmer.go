cfa055d422b2f4091e81d398e4e18cbea26670b4
shiryay <shiryay@internet.ru>
2024-10-16 21:20:56 +0300

    added stemmer module

---
+++ stemmer/stemmer.go
@@ -0,0 +1,69 @@
+package stemmer
+
+import (
+    "fmt"
+    "github.com/kljensen/snowball"
+    "strings"
+)
+
+func getStem(originalWord, language string) (string, error) {
+    stemmedWord, err := snowball.Stem(originalWord, language, true)
+    if err != nil {
+        return "", err
+    } else {
+        return stemmedWord, nil
+    }
+}
+
+func stemWord(originalWord, language string) (string, error) {
+    stem, err := getStem(originalWord, language)
+    if err != nil {
+        return "", err
+    } else {
+        insert := stem + "|"
+        split := strings.Replace(originalWord, stem, insert, 1)
+        return split, nil
+    }
+}
+
+func stemPhrase(originalPhrase, language string) (string, error) {
+    words := strings.Split(originalPhrase, " ")
+    splitWords := make([]string, len(words))
+    for _, word := range words {
+        splitWord, err := stemWord(word, language)
+        if err != nil {
+            return "", err
+        } else {
+            splitWords = append(splitWords, splitWord)
+        }
+    }
+    return strings.Join(splitWords, " "), nil
+}
+
+func stemLineWithTabs(line, language string) (string, error) {
+    lineParts := strings.Split(line, "\t")
+    newLineParts := make([]string, 0)
+    stemmedPart, err := stemPhrase(lineParts[0], language)
+    if err != nil {
+        return "", err
+    } else {
+        newLineParts = append(newLineParts, stemmedPart)
+        newLineParts = append(newLineParts, lineParts[1])
+        return strings.Join(newLineParts, "\t"), nil
+    }
+}
+
+func stemText(text, language string) (string, error) {
+    sep := "\n" // may need to be different for web, \r\n or something like that
+    lines := strings.Split(text, sep)
+    newLines := make([]string, 0)
+    for _, line := range lines {
+        stemmedLine, err := stemLineWithTabs(line, language)
+        if err != nil {
+            return "", err
+        } else {
+            newLines = append(newLines, stemmedLine)
+        }
+    }
+    return strings.Join(newLines, sep), nil
+}
